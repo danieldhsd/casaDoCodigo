9c76dec4deab46d10843b9b03f0b37cde4862942
Daniel Dutra <danieldhsd@gmail.com>
2020-04-23 11:49:14 -0300

    Recuperando a capa do livro

--- src/main/java/org/casadocodigo/infra/FileSaver.java
+++ src/main/java/org/casadocodigo/infra/FileSaver.java
@@ -1,11 +1,19 @@
 package org.casadocodigo.infra;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.nio.file.Path;
 
 import javax.servlet.http.Part;
 
 public class FileSaver {
-	private static final String SERVER_PATH = "/home/danieldhsd/casadocodigo";
+	public static final String SERVER_PATH = "/home/danieldhsd/casadocodigo";
 
 	public String write(Part arquivo, String path) {
 		try {
@@ -14,6 +22,28 @@ public class FileSaver {
 			
 			return relativePath;
 		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void transfer(Path source, OutputStream outputStream) {
+		
+		try {
+			FileInputStream inputStream = new FileInputStream(source.toFile());
+			try(ReadableByteChannel inputChannel = Channels.newChannel(inputStream);
+					WritableByteChannel outputChannel = Channels.newChannel(outputStream)){
+				ByteBuffer buffer = ByteBuffer.allocateDirect(1024 * 10);
+				
+				while(inputChannel.read(buffer) != -1) {
+					buffer.flip();
+					outputChannel.write(buffer);
+					buffer.clear();
+				}
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+			
+		} catch (FileNotFoundException e) {
 			throw new RuntimeException(e);
 		}
 	}
